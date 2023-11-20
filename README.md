3eac946f085c806680afd15afee2f43540a7e8d0
Senne Malfliet <senne.malfliet@gmail.com>
2023-11-20 23:06:20 +0100

    new site

--- README.md
+++
@@ -1,1 +0,0 @@
-# sennemalfliet.github.io
---
+++ main.js
@@ -0,0 +1,14 @@
+// Mouse blur
+const blob = document.getElementById("blob")
+	
+document.body.onpointermove = event => {
+	const { clientX, clientY } = event;
+
+	blob.animate({
+		left: `${clientX}px`,
+		top: `${clientY}px`
+		}, {
+		duration: 3000,
+		fill: "forwards"
+	});
+}
